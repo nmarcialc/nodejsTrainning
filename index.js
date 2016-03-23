093ba811db9c05a6b1e753cfa7200c19ab28c599
Nataniel Marcial <nataniel_natas@hotmail.com>
2016-03-23 23:20:53 +0000

    add new files structure

---
+++ index.js
@@ -0,0 +1,127 @@
+var config = require('./lib/backupConfig');
+ var myArgs  = require("yargs")
+ .command(
+     'backup',
+     'generate backup from bridallive',
+     {
+         host :{
+             alias:'h',
+             demand:true,
+             describe : 'Bridallive host',
+             type: 'string',
+             requiresArg:true
+         },
+         key :{
+             alias:'k',
+             demand:true,
+             describe : 'Bridallive apip key from bridallive',
+             type: 'string',
+             requiresArg:true
+         },
+         retailerId :{
+             alias:'r',
+             demand:true,
+             describe : 'Bridallive retailer id',
+             type: 'string',
+             requiresArg:true
+         },
+         destination :{
+             alias:'d',
+             demand:true,
+             describe : 'Destination to save de backup',
+             type: 'string',
+             requiresArg:true
+         }
+     }
+     
+  )
+  .command(
+     'purge',
+     'generate backup from bridallive',
+     {
+         host :{
+             alias:'h',
+             demand:true,
+             describe : 'Bridallive host',
+             type: 'string',
+             requiresArg:true
+         },
+         key :{
+             alias:'k',
+             demand:true,
+             describe : 'Bridallive apip key from bridallive',
+             type: 'string',
+             requiresArg:true
+         },
+         retailerId :{
+             alias:'r',
+             demand:true,
+             describe : 'Bridallive retailer id',
+             type: 'string',
+             requiresArg:true
+         }
+     }
+     
+  )
+  .command(
+     'restore',
+     'restore backup to bridallive',
+     {
+         host :{
+             alias:'h',
+             demand:true,
+             describe : 'Bridallive host',
+             type: 'string',
+             requiresArg:true
+         },
+         key :{
+             alias:'k',
+             demand:true,
+             describe : 'Bridallive apip key from bridallive',
+             type: 'string',
+             requiresArg:true
+         },
+         retailerId :{
+             alias:'r',
+             demand:true,
+             describe : 'Bridallive retailer id',
+             type: 'string',
+             requiresArg:true
+         },
+         source :{
+             alias:'s',
+             demand:true,
+             describe : 'source folder',
+             type: 'string',
+             requiresArg:true
+         }
+     }
+     
+  )
+  .usage('describe funtions')
+ .help()
+ .demand(1)
+ .strict()
+ .argv;
+ 
+ 
+ 
+ switch (myArgs._[0]) {
+   case 'backup':
+     console.log('create a backup');
+     break;
+   case 'purge':
+     console.log('purge database');
+     break;
+   case 'restore':
+     console.log('restore database');
+     break;
+   default:
+     console.log('no command to execute');
+     break;
+     
+ }
+ 
+config.forEach(function(entry) {
+    console.log(entry);
+});
