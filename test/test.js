fc1474edd8ce57388891f81151d498a7adf30972
Gregorio <gregorio.montoya@sonorasoft.com>
2016-03-29 01:21:19 +0000

    my first test with chai

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+describe('A basic test',function (){
+    it('i should pass when everything is oka', function(){
+        expect(true).to.be.true;
+        
+        
+    })
+    
+    
+})
+   
